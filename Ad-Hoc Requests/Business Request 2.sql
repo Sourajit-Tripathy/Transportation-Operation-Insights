b9fc6833ebbe5d7d5dab864b366ab5d5d58b5548
Sourajit-Tripathy <tripathyjeet111@gmail.com>
2024-12-14 15:29:08 +0530

    Create Business Request 2.sql

---
+++ Ad-Hoc Requests/Business Request 2.sql
@@ -0,0 +1,12 @@
+-- % contribution for all trips by city -- 
+
+with cte (city, totalcount) as
+ (select c.city_name , count(f.trip_id)
+from dim_city c join fact_trips f 
+on c.city_id = f.city_id
+group by c.city_name
+),
+ sumofcount as (select sum(totalcount) as sumx from cte)
+ select city,totalcount,(totalcount*100/sumx)as percentage_contribution from sumofcount,cte
+ order by percentage_contribution desc ;
+ 
